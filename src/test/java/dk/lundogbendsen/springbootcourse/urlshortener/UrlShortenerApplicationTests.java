ca645fcd3a936191aca71891db7e718fee7e317d
cvw <cvwjensen@gmail.com>
2021-02-09 09:13:40 +0100

    naked springboot project

---
+++ src/test/java/dk/lundogbendsen/springbootcourse/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.lundogbendsen.springbootcourse.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
