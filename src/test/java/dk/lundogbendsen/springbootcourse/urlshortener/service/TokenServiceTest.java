e795941ded3975e4431d456217b6c251b0ada6fb
cvw <cvw@energisk.eu>
2021-07-28 07:55:20 +0200

    Upgraded to 2.5.3

--- src/test/java/dk/lundogbendsen/springbootcourse/urlshortener/service/TokenServiceTest.java
+++ src/test/java/dk/lundogbendsen/springbootcourse/urlshortener/service/TokenServiceTest.java
@@ -1,19 +1,31 @@
 package dk.lundogbendsen.springbootcourse.urlshortener.service;
 
+import dk.lundogbendsen.springbootcourse.urlshortener.model.Token;
 import dk.lundogbendsen.springbootcourse.urlshortener.model.User;
+import dk.lundogbendsen.springbootcourse.urlshortener.repositories.TokenRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.net.URI;
 import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TokenServiceTest {
+    @Mock TokenRepository tokenRepository;
     @InjectMocks
     TokenService tokenService;
     private User user = User.builder().username("username").password("password").build();
@@ -31,8 +43,9 @@ class TokenServiceTest {
     @Test
     @DisplayName("create token that already exists (fails)")
     public void testCreateTokenThatAlreadExists() {
+        final Token token = Token.builder().token("token1").build();
+        when(tokenRepository.findById("token1")).thenReturn(java.util.Optional.ofNullable(token));
         try {
-            tokenService.create("token1", "https://dr.dk", null, user);
             tokenService.create("token1", "https://dr.dk", null, user);
             fail();
         } catch (Exception e) {
