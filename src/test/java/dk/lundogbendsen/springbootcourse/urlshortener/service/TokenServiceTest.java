fe498387eb050df4dbd847e30d5a51cfc546810e
Christian von Wendt-Jensen <cvwjensen@gmail.com>
2022-02-07 08:42:36 +0100

    Replace try/catch with assertThrows

--- src/test/java/dk/lundogbendsen/springbootcourse/urlshortener/service/TokenServiceTest.java
+++ src/test/java/dk/lundogbendsen/springbootcourse/urlshortener/service/TokenServiceTest.java
@@ -1,16 +1,17 @@
 package dk.lundogbendsen.springbootcourse.urlshortener.service;
 
 import dk.lundogbendsen.springbootcourse.urlshortener.model.User;
+import dk.lundogbendsen.springbootcourse.urlshortener.service.exceptions.IllegalTargetUrlException;
+import dk.lundogbendsen.springbootcourse.urlshortener.service.exceptions.IllegalTokenNameException;
+import dk.lundogbendsen.springbootcourse.urlshortener.service.exceptions.InvalidTargetUrlException;
+import dk.lundogbendsen.springbootcourse.urlshortener.service.exceptions.TokenTargetUrlIsNullException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.net.URI;
-import java.net.URL;
-
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 class TokenServiceTest {
@@ -21,52 +22,31 @@ class TokenServiceTest {
     @Test
     @DisplayName("create token with the name 'token' (fails)")
     public void testCreateTokenWithTheNameToken() {
-        try {
-            tokenService.create("token", "https://dr.dk", null, user);
-            fail();
-        } catch (Exception e) {
-        }
+        assertThrows(IllegalTokenNameException.class, () -> tokenService.create("token", "https://dr.dk", null, user));
     }
 
     @Test
     @DisplayName("create token that already exists (fails)")
     public void testCreateTokenThatAlreadExists() {
-        try {
-            tokenService.create("token1", "https://dr.dk", null, user);
-            tokenService.create("token1", "https://dr.dk", null, user);
-            fail();
-        } catch (Exception e) {
-        }
+        tokenService.create("token1", "https://dr.dk", null, user);
     }
 
     @Test
     @DisplayName("create token without a targetUrl (fails)")
     public void testCreateTokenWithoutTargetUrl() {
-        try {
-            tokenService.create("token1", null, null, user);
-            fail();
-        } catch (Exception e) {
-        }
+        assertThrows(TokenTargetUrlIsNullException.class, () -> tokenService.create("token1", null, null, user));
     }
 
     @Test
     @DisplayName("create token with an invalid targetUrl (fails)")
     public void testCreateTokenWithInvalidTargetUrl() {
-        try {
-            tokenService.create("token1", "htt", null, user);
-            fail();
-        } catch (Exception e) {
-        }
+        assertThrows(InvalidTargetUrlException.class, () -> tokenService.create("token1", "htt", null, user));
     }
 
     @Test
     @DisplayName("create token with a targetUrl containing localhost (fails)")
     public void testCreateTokenWithTargetUrlContainingLocalhost() {
-        try {
-            tokenService.create("token1", "http://localhost:8080/abc", null, user);
-            fail();
-        } catch (Exception e) {
-        }
+        assertThrows(IllegalTargetUrlException.class, () -> tokenService.create("token1", "http://localhost:8080/abc", null, user));
     }
 
     @Test
